305c2cbe22e7af36226507ad9eafa1ebd3513ae9
A11en4 <lidonghit@126.com>
2018-08-17 22:50:35 +0800

    git commit

--- js/Untitled.py
+++ js/Untitled.py
@@ -17,10 +17,14 @@
 i = int(raw_input('input:'))
 m = [1000000,600000,400000,200000,100000,0]
 t = [0.01,0.015,0.03,0.05,0.075,0.1]
+
+
 r = 0
 for s in range(0,6):#循环列表元素数
     if i>m[s]:#判断输入的元素
         r+=(i-m[s])*t[s]
         print (i-m[s])*t[s]
         i=m[s]
+print r
+print r
 print r
